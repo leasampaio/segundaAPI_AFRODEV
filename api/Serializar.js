a5e2ccc5466e059cfea6e24693a0482f821993fa
Lea Sampaio <lealisboas@gmail.com>
2021-04-10 09:09:30 -0300

    Aula sabado

--- api/Serializar.js
+++ api/Serializar.js
@@ -2,7 +2,7 @@ const FormatoInvalido = require('./erros/Formatoinvalido');
 
 class Serializar {
     json (dados){
-        JSON.stringify(dados)
+        return JSON.stringify(dados)
     }
     transformar(dados){
         if(this.contentType !== 'application/json'){
@@ -19,12 +19,13 @@ class Serializar {
             }
             
         });
+        return camposFiltrados;
     }
     filtrar(dados){
         let dadosFiltrados = this.filtrarCampos(dados);
 
         if(Array.isArray(dados)){
-            dadosFiltrados.map((dados)=>{
+            dadosFiltrados = dados.map((dados)=>{
                 return this.filtrarCampos(dados);
 
             })
